583a55ee780403688c337416950ec3e3f35d94fc
gaurav613 <gigs612@gmail.com>
2020-04-02 22:06:35 -0400

    update create table statements

--- queries/schema.sql
+++ queries/schema.sql
@@ -1,97 +1,75 @@
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Employees`
---
-
-DROP TABLE IF EXISTS `Employees`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Employees` (
-  `EmployeeID` smallint NOT NULL AUTO_INCREMENT,
-  `JobTitle` varchar(150) NOT NULL,
-  `Name` varchar(150) NOT NULL,
-  `AccessLevel` enum('Full','Guest') DEFAULT NULL,
-  PRIMARY KEY (`EmployeeID`)
+/*query to create employees table*/
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+ `EmployeeID` smallint(6) NOT NULL AUTO_INCREMENT,
+ `JobTitle` varchar(150) NOT NULL,
+ `Name` varchar(150) NOT NULL,
+ `AccessLevel` enum('Full','Guest') DEFAULT NULL,
+ `isApprover` tinyint(1) NOT NULL DEFAULT 0,
+ PRIMARY KEY (`EmployeeID`)
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `Posts`
---
-
-DROP TABLE IF EXISTS `Posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Posts` (
-  `PostID` smallint NOT NULL AUTO_INCREMENT,
-  `IsScheduled` bit(1) NOT NULL DEFAULT b'0',
-  `Status` enum('Draft','Live','Deleted') DEFAULT 'Draft',
-  `Content` text,
-  PRIMARY KEY (`PostID`)
+/*query to create posts table*/
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+ `PostID` smallint(6) NOT NULL AUTO_INCREMENT,
+ `IsScheduled` tinyint(1) NOT NULL DEFAULT 0,
+ `Status` enum('Draft','Live','Deleted') DEFAULT 'Draft',
+ `Content` text DEFAULT NULL,
+ `approved` tinyint(1) NOT NULL DEFAULT 0,
+ PRIMARY KEY (`PostID`)
 );
 
-/*!40101 SET character_set_client = @saved_cs_client */;
+/*query to create users table*/
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+ `userID` smallint(6) NOT NULL AUTO_INCREMENT,
+ `username` varchar(255) NOT NULL,
+ `password` varchar(255) NOT NULL,
+ `email` varchar(255) NOT NULL,
+ `EmployeeID` smallint(6) NOT NULL,
+ PRIMARY KEY (`userID`),
+ KEY `EmployeeID` (`EmployeeID`),
+ CONSTRAINT `users_ibfk_1` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`)
+) ;
 
---
--- Table structure for table `SocialMediaSites`
---
-
+/*query to create SocialMediaSites table*/
 DROP TABLE IF EXISTS `SocialMediaSites`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `SocialMediaSites` (
-  `SiteURL` varchar(150) NOT NULL,
-  `AccessToken` mediumtext NOT NULL,
-  PRIMARY KEY (`SiteURL`)
+CREATE TABLE `socialmediasites` (
+ `SiteURL` varchar(150) NOT NULL,
+ `AccessToken` mediumtext NOT NULL,
+ PRIMARY KEY (`SiteURL`)
 );
 
-DROP TABLE IF EXISTS `ApprovedEmployees`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ApprovedEmployees` (
-  `EmployeeID` smallint NOT NULL,
-  `SiteURL` varchar(150) NOT NULL,
-  `IsSubscribed` bit(1) NOT NULL DEFAULT b'1',
-  PRIMARY KEY (`EmployeeID`,`SiteURL`),
-  KEY `SiteURL` (`SiteURL`),
-  CONSTRAINT `approvedemployees_ibfk_1` FOREIGN KEY (`EmployeeID`) REFERENCES `Employees` (`EmployeeID`),
-  CONSTRAINT `approvedemployees_ibfk_2` FOREIGN KEY (`SiteURL`) REFERENCES `SocialMediaSites` (`SiteURL`)
-);
-
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `LivePosts`
---
-
+/*query to create LivePosts table*/
 DROP TABLE IF EXISTS `LivePosts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `LivePosts` (
-  `SiteURL` varchar(150) NOT NULL,
-  `PostID` smallint NOT NULL,
-  `EmployeeID` smallint NOT NULL,
-  `PostUploadTime` datetime NOT NULL,
-  PRIMARY KEY (`SiteURL`,`PostID`,`EmployeeID`),
-  KEY `PostID` (`PostID`),
-  KEY `EmployeeID` (`EmployeeID`),
-  CONSTRAINT `liveposts_ibfk_1` FOREIGN KEY (`PostID`) REFERENCES `Posts` (`PostID`),
-  CONSTRAINT `liveposts_ibfk_2` FOREIGN KEY (`EmployeeID`) REFERENCES `Employees` (`EmployeeID`),
-  CONSTRAINT `liveposts_ibfk_3` FOREIGN KEY (`SiteURL`) REFERENCES `SocialMediaSites` (`SiteURL`)
+CREATE TABLE `liveposts` (
+ `SiteURL` varchar(150) NOT NULL,
+ `PostID` smallint(6) NOT NULL,
+ `EmployeeID` smallint(6) NOT NULL,
+ `PostUploadTime` datetime NOT NULL,
+ PRIMARY KEY (`SiteURL`,`PostID`,`EmployeeID`),
+ KEY `PostID` (`PostID`),
+ KEY `EmployeeID` (`EmployeeID`),
+ CONSTRAINT `liveposts_ibfk_1` FOREIGN KEY (`PostID`) REFERENCES `posts` (`PostID`),
+ CONSTRAINT `liveposts_ibfk_2` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`),
+ CONSTRAINT `liveposts_ibfk_3` FOREIGN KEY (`SiteURL`) REFERENCES `socialmediasites` (`SiteURL`)
+);
+
+/*query to create ApprovedEmployees table*/
+DROP TABLE IF EXISTS `ApprovedEmployees`;
+CREATE TABLE `approvedemployees` (
+ `EmployeeID` smallint(6) NOT NULL,
+ `SiteURL` varchar(150) NOT NULL,
+ `IsSubscribed` bit(1) NOT NULL DEFAULT b'1',
+ PRIMARY KEY (`EmployeeID`,`SiteURL`),
+ KEY `SiteURL` (`SiteURL`),
+ CONSTRAINT `approvedemployees_ibfk_1` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`),
+ CONSTRAINT `approvedemployees_ibfk_2` FOREIGN KEY (`SiteURL`) REFERENCES `socialmediasites` (`SiteURL`)
 );
 
 
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-31 22:27:19
+	
+
